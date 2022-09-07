186f407e101d89c0126efca53a8378555d80872d
T.J. Eckman <jeek@jeek.net>
2022-09-06 20:31:26 -0400

    Update burnout.js

--- burnout/burnout.js
+++ burnout/burnout.js
@@ -627,6 +627,72 @@ class Corp {
 		}
 	}
 }
+/*   [Industries.Food]: {
+    Quality: 0.7,
+    Durability: 0.1,
+    Aesthetics: 0.2,
+  },
+  [Industries.Tobacco]: {
+    Quality: 0.4,
+    Durability: 0.2,
+    Reliability: 0.2,
+    Aesthetics: 0.2,
+  },
+  [Industries.Pharmaceutical]: {
+    Quality: 0.2,
+    Performance: 0.2,
+    Durability: 0.1,
+    Reliability: 0.3,
+    Features: 0.2,
+  },
+  [Industries.Computer]: {
+    Quality: 0.15,
+    Performance: 0.25,
+    Durability: 0.25,
+    Reliability: 0.2,
+    Aesthetics: 0.05,
+    Features: 0.1,
+  },
+  Computer: {
+    //Repeat
+    Quality: 0.15,
+    Performance: 0.25,
+    Durability: 0.25,
+    Reliability: 0.2,
+    Aesthetics: 0.05,
+    Features: 0.1,
+  },
+  [Industries.Robotics]: {
+    Quality: 0.1,
+    Performance: 0.2,
+    Durability: 0.2,
+    Reliability: 0.2,
+    Aesthetics: 0.1,
+    Features: 0.2,
+  },
+  [Industries.Software]: {
+    Quality: 0.2,
+    Performance: 0.2,
+    Reliability: 0.2,
+    Durability: 0.2,
+    Features: 0.2,
+  },
+  [Industries.Healthcare]: {
+    Quality: 0.4,
+    Performance: 0.1,
+    Durability: 0.1,
+    Reliability: 0.3,
+    Features: 0.1,
+  },
+  [Industries.RealEstate]: {
+    Quality: 0.2,
+    Durability: 0.25,
+    Reliability: 0.1,
+    Aesthetics: 0.35,
+    Features: 0.1,
+  },*/
+
+
 
 let possibleArguments = [
 	['help', false],
